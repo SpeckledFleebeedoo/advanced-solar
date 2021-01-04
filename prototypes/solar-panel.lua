0b0e4dedc41cb2591f627eb4bde1d61e15fa95e0
SpeckledFleebeedoo <49042682+SpeckledFleebeedoo@users.noreply.github.com>
2021-01-04 16:34:59 +0100

    Update to 1.1.5

--- prototypes/solar-panel.lua
+++ prototypes/solar-panel.lua
@@ -1,17 +1,28 @@
+local productionsetting = settings.startup["advanced-solar-production"].value
+local costfactor = math.floor(productionsetting / 60)
+
 data:extend(
 {
     {
         type = "recipe",
         name = "advanced-solar",
         enabled = false,
-        ingredients = {{"advanced-circuit", 10}, {"steel-plate",10}, {"solar-panel",4}},
-        energy_required = 20,
+        ingredients = {{"advanced-circuit", 20}, {"steel-plate",10}, {"solar-panel", costfactor}},
+        energy_required = costfactor * 5,
         result = "advanced-solar"
     },
     {
         type = "item",
         name = "advanced-solar",
-        icon = "__base__/graphics/icons/solar-panel.png",
+        icons = {
+            {
+                icon = "__base__/graphics/icons/solar-panel.png",
+            },
+            {
+                icon = "__base__/graphics/icons/solar-panel.png",
+                tint = {0, 0, 1, 0.001}
+            }
+        },
         icon_size = 64,
         place_result = "advanced-solar",
         stack_size = 100,
@@ -24,7 +35,7 @@ data:extend(
         icon = "__base__/graphics/icons/solar-panel.png",
         icon_size = 64,
         flags = {"placeable-neutral", "player-creation"},
-        minable = {haredness = 0.1, mining_time = 0.2, result = "advanced-solar"},
+        minable = {hardness = 0.1, mining_time = 0.2, result = "advanced-solar"},
         max_health = 800,
         corpse = "solar-panel-remnants",
         collision_box = {{-2.4, -2.4}, {2.4, 2.4}},
@@ -55,16 +66,25 @@ data:extend(
                 shift = util.by_pixel(10,7)
             }
         },
-        production = "1MW"
+        production = tostring(productionsetting).."kW"
     },
+    
     {
         type = "technology",
         name = "advanced-solar",
-        icon = "__base__/graphics/technology/solar-energy.png",
-        icon_size = 128,
+        icons = {
+            {
+                icon = "__base__/graphics/technology/solar-energy.png",
+            },
+            {
+                icon = "__base__/graphics/technology/solar-energy.png",
+                tint = {0, 0, 1, 0.001}
+            }
+        },
+        icon_size = 256, icon_mipmaps = 4,
         effects = {{type = "unlock-recipe", recipe = "advanced-solar"}},
-        prerequisites = {"solar-energy", "advanced-electronics", "chemical-science-pack"},
-        unit = {count=200, ingredients = {{"automation-science-pack", 1},{"logistic-science-pack", 1},{"chemical-science-pack",1}}, time=30}
+        prerequisites = {"solar-energy", "advanced-electronics", "production-science-pack"},
+        unit = {count=200, ingredients = {{"automation-science-pack", 1},{"logistic-science-pack", 1},{"chemical-science-pack",1},{"production-science-pack",1}}, time=30}
     }
 }
 )
---
+++ settings.lua
@@ -0,0 +1,10 @@
+data:extend({
+    {
+        type = "int-setting",
+        name = "advanced-solar-production",
+        localised_description = "How much energy each advanced solar panel produces in kW. (Minimum 167, default = 1000)",
+        default_value = 1000,
+        minimum_value = 167,
+        setting_type = "startup"
+    }
+})
